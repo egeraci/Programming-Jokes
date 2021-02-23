0bc5262f4827903e8e5d32d0fd47dbd649d7f1b2
Elizabeth Geraci <egeraci@bsd220.org>
2021-02-23 15:58:54 -0600

    We got it working!

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingalert = false
     var body: some View {
         NavigationView
         
@@ -35,11 +36,55 @@ label: {
         .onAppear(perform: {
             getJokes()
         })
+        .alert(isPresented: $showingalert, content: {
+
+                        Alert(title: Text("Loading Error"),
+
+                              message: Text("There was a problem loading the data"),
+
+                              dismissButton: .default(Text("OK")))
+
+                })
    
     }
     func getJokes() {
 
     let apikey = "?rapidapi-key=9ffc0481e6msh0b0a5178fa7b7a1p135476jsn3edbce5e740f"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apikey)"
+
+        if let url = URL(string: query)
+        {
+
+            if let data = try? Data(contentsOf: url)
+            {
+
+                let json = try! JSON(data: data)
+
+                if json["success"] == true
+                {
+
+                    let contents = json["body"].arrayValue
+
+                    for item in contents
+                    {
+
+                        let setup = item["setup"].stringValue
+
+                        let punchline = item["punchline"].stringValue
+
+                        let joke = Joke(setup: setup, punchline: punchline)
+
+                        jokes.append(joke)
+
+                    }
+                    return
+
+                }
+
+            }
+
+        }
+
       }
 
 
@@ -55,75 +100,3 @@ struct ContentView_Previews: PreviewProvider {
         var setup: String
         var punchline: String
     }
-let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
-        if let url = URL(string: query)
-        {
-
-            if let data = try? Data(contentsOf: url)
-        {
-
-                let json = try! JSON(data: data)
-
-                if json["success"] == true
-                {
-
-                    let contents = json["body"].arrayValue
-
-                }
-
-            }
-
-        }
-for item in contents
-                    {
-
-                        let setup = item["setup"].stringValue
-
-                        let punchline = item["punchline"].stringValue
-
-                        let joke = Joke(setup: setup, punchline: punchline)
-
-                        jokes.append(joke)
-
-                    }
-func getJokes()
-            {
-
-       let apiKey = "?rapidapi-key=(paste your Rapid API key here)"
-
-       let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
-
-       if let url = URL(string: query)
-       {
-
-           if let data = try? Data(contentsOf: url)
-           {
-
-               let json = try! JSON(data: data)
-
-               if json["success"] == true
-               {
-
-                   let contents = json["body"].arrayValue
-
-                   for item in contents
-                   {
-
-                       let setup = item["setup"].stringValue
-
-                       let punchline = item["punchline"].stringValue
-
-                       let joke = Joke(setup: setup, punchline: punchline)
-
-                       jokes.append(joke)
-
-                   }
-
-               }
-
-           }
-
-       }
-
-   }
-
